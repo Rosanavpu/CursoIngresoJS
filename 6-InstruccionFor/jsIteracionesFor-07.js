2c50c3f36e3483be5226aa52428f66940208a934
Rosana Peralta <rosana@wearecapicua.com>
2021-12-12 13:43:42 -0300

    Sexta carpeta con ejercicios resueltos

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,8 @@
-function mostrar()
-{
-
-	alert("ok");
-
-
-}//FIN DE LA FUNCIÓN
+function mostrar() {
+	var n = prompt('Dame un número');
+	for (i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			document.write(i + ' ');
+		}
+	}
+} //FIN DE LA FUNCIÓN
